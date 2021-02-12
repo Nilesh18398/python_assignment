55977520ae5e9c7a1bc33e5242abbeee11e5bde1
Nilesh18398 <40518862+Nilesh18398@users.noreply.github.com>
2021-02-12 19:11:03 +0530

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,15 @@
+def isPrime(num):
+    if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               print(num,"is not a prime number")  
+               break  
+       else:  
+           print(num,"is a prime number")  
+             
+    else:  
+       print(num,"is not a prime number")  
+  
+isPrime(11)
+isPrime(15) 
+
